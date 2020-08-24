15df8427df372cf4500bfbbdb75ea8a2ec488c50
Claudio Junior <claudio.macedo@aparecida.go.gov.br>
2020-08-24 14:56:45 -0300

    refactor: move form to a separate file

---
+++ src/presentation/components/form-status/form-status.tsx
@@ -0,0 +1,15 @@
+import React, { memo } from 'react'
+import Styles from './form-status-styles.scss'
+import Spinner from '@/presentation/components/spinner/spinner'
+
+const FormStatus: React.FC = () => {
+  return (
+    <div className={Styles.errorWrap}>
+      <Spinner className={Styles.spinner}/>
+      <span className={Styles.error}>Error</span>
+    </div>
+
+  )
+}
+
+export default memo(FormStatus)
