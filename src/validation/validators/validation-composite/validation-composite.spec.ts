34f967a690173c434c853a164afde85149441cd7
Cláudio Sousa de Macedo Júnior <claudio.macedo@aparecida.go.gov.br>
2020-09-02 09:08:35 -0300

    test: ensure ValidationComposite returns error if any validation fails

---
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -0,0 +1,16 @@
+import { ValidationComposite } from './validation-composite'
+import { FieldValidateSpy } from '../test/mock-field-validation'
+
+describe('ValidationComposite', () => {
+  test('Should retunr error if any validation fails', () => {
+    const fieldValidationSpy = new FieldValidateSpy('any_field')
+    const fieldValidationSpy2 = new FieldValidateSpy('any_field')
+    fieldValidationSpy2.error = new Error('any_error_message')
+    const sut = new ValidationComposite([
+      fieldValidationSpy,
+      fieldValidationSpy2
+    ])
+    const error = sut.validate('any_field', 'any_value')
+    expect(error).toBe('any_error_message')
+  })
+})
