09911720343a4c42c0de39a8d831af5f20980999
Cláudio Sousa de Macedo Júnior <claudio.macedo@aparecida.go.gov.br>
2020-09-02 09:10:57 -0300

    test: ensure ValidationComposite returns the first error found

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -4,13 +4,14 @@ import { FieldValidateSpy } from '../test/mock-field-validation'
 describe('ValidationComposite', () => {
   test('Should retunr error if any validation fails', () => {
     const fieldValidationSpy = new FieldValidateSpy('any_field')
+    fieldValidationSpy.error = new Error('first_error_message')
     const fieldValidationSpy2 = new FieldValidateSpy('any_field')
-    fieldValidationSpy2.error = new Error('any_error_message')
+    fieldValidationSpy2.error = new Error('secound_error_message')
     const sut = new ValidationComposite([
       fieldValidationSpy,
       fieldValidationSpy2
     ])
     const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('any_error_message')
+    expect(error).toBe('first_error_message')
   })
 })
