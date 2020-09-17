494289748bca56702dc88470f52671ddffe225f8
Cláudio Sousa de Macedo Júnior <claudio.macedo@aparecida.go.gov.br>
2020-09-17 13:35:18 -0300

    test: ensure login begin with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,14 @@
 describe('Login', () => {
-  it('Shuld load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+  it('Shuld load with correct initial state', () => {
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo obrigatorio')
+      .should('contain.text', '🔴')
+    cy.getByTestId('password-status').should('have.attr', 'title', 'Campo obrigatorio')
+      .should('contain.text', '🔴')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
