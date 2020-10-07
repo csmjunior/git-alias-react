570c19664fdab40bc2763f1a9c347ad27543a4b0
Cláudio Sousa de Macedo Júnior <claudio.macedo@aparecida.go.gov.br>
2020-10-07 14:41:06 -0300

    refactor: move SurveyList code to components

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,5 +1,5 @@
 import { Footer, Header } from '@/presentation/components'
-import { SurveyItem, SurveyItemEmpty } from '@/presentation/pages/survey-list/components'
+import { SurveyContext, SurveyListItem, SurveyError } from '@/presentation/pages/survey-list/components'
 import Styles from './survey-list-styles.scss'
 import { LoadSurveyList } from '@/domain/usecases/load-survey-list'
 import React, { useEffect, useState } from 'react'
@@ -24,18 +24,9 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
       <Header/>
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
-        {state.error
-          ? <div>
-            <span data-testid="error">{state.error}</span>
-            <button>Recarregar</button>
-          </div>
-          : <ul data-testid="survey-list">
-            {state.surveys.length
-              ? state.surveys.map((survey: SurveyModel) => <SurveyItem key={survey.id} survey={survey}/>)
-              : <SurveyItemEmpty />
-            }
-          </ul>
-        }
+        <SurveyContext.Provider value={{ state, setState }}>
+          {state.error ? <SurveyError/> : <SurveyListItem />}
+        </SurveyContext.Provider>
       </div>
       <Footer />
     </div>
