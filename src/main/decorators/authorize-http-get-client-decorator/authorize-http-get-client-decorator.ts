d1a3819d337503d77c22f7c11fc29135d30402ae
Cláudio Sousa de Macedo Júnior <claudio.macedo@aparecida.go.gov.br>
2020-10-15 15:52:18 -0300

    feat: ensure AuthorizeHttpGetClientDecorator merge token to HttpGetClient

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
@@ -9,11 +9,11 @@ export class AuthorizeHttpGetClientDecorator implements HttpGetClient {
 
   async get (params: HttpGetParams): Promise<HttpResponse> {
     const account = this.getStorage.get('account')
-    if (account.accessToken) {
+    if (account?.accessToken) {
       Object.assign(params, {
-        headers: {
+        headers: Object.assign(params.headers || {},{
           'x-access-token': account.accessToken
-        }
+        })
       })
     }
     await this.httpGetClient.get(params)
